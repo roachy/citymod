6975e0d31de56835b874930960e45db34a2edb5f
Seraph <seraph14@hotmail.com>
2019-08-15 17:41:07 +0200

    Added a few comments

--- src/libraries/sh_item.lua
+++ src/libraries/sh_item.lua
@@ -34,25 +34,27 @@ end
 -- Registering a new item
 function CityMod.Item:Register(data)
 
+    -- Check if an ID was specified to the item (the parameter of :New on item creation)
     if (not data.Id) then
         print("No ID specified to item in file "..debug.getinfo(2, "S").source:sub(2))
         return
     end
 
-    -- Set the category of the item
+    -- Check if the item has a valid category
     if (data.Category == "None") then
         print("Item with name "..data.Name.." does not have a category")
         return
     end
 
-    -- Get the category
+    -- Get the category of the item
     local category = CityMod.Item.Category:Get(data.Category)
 
+    -- Check if the category was found
     if (category == nil) then
         print("Category with name "..data.Category.." does not exist")
     end
 
-    -- Set metatable to the category
+    -- Set metatable to the category to make it able to access its baseclass
     setmetatable(data, category)
 
     print(data:Execute())
